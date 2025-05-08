281b815a89fa0ba475f3b00ffff0ab76a51db95d
justinmateo <shoto0387@gmail.com>
2025-05-08 09:58:23 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return ('Justin Matthew Bolotaulo'
+            'BSIT 3 - 1st 25'
+            'ITE 322 - System Integration and Architecture 1'
+            'Semi-Final Exam')
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
